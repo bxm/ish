43b147bdab34d596075ea105ba578c2a5958aa97
bxm <blues.xxx.man@gmail.com>
2020-11-24 08:44:49 +0000

    todo notes

--- scan.sh
+++ scan.sh
@@ -7,6 +7,8 @@ multi_thread(){
        $0 do $* % \
     | sort -V
   true
+# flip params around so ip comes first 
+# then we could have multiple ports
 }
 
 port(){
@@ -33,13 +35,21 @@ EOF
   exit
 }
 
+parse_range() {
+  # split off final octet
+  #   if hyphen present is range otherwise single
+  # generate three arrays, subnet, start octet, end octet
+}
+
 main() {
   if [ "$1" = do ] ; then shift ; "$@" ; return ; fi
 
   while [ $# -gt 0 ] ; do
     case "$1" in
+# do it like port:22 thus range is implied, allowing multiples
       (port) ACTION="$1" ; PARAM="$2" ; shift ;;
       (ping) ACTION="$1" ;;
+# allow multiple ranges via assoc array
       (-r?*|--range=?*) RANGE="${1#-r}" ; RANGE="${RANGE#*=}" ;;
       (-r  |--range   ) RANGE="${2}" ; shift ;;
       (*) usage ;;
