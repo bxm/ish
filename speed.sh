af44a68fedbdb7d686bc81c3be68214a7c9be427
bxm <blues.xxx.man@gmail.com>
2021-08-02 07:52:15 +0000

    add in iterations

--- speed.sh
+++ speed.sh
@@ -1,3 +1,9 @@
-ping 8.8.8.8 -c ${1:-5} \
+SMOOTH=${1:-3}
+ITER=${2:-5}
+
+while [ ${ITER} -gt 0 ] ; do
+ping 8.8.8.8 -c ${SMOOTH} \
   | grep -oE time=[^\ ]+ \
   | awk -F= '{n+=$NF ; i+=1} END {print n/i}'
+: $((ITER-=1))
+done
