da07190213f9efc0e76a3f358922d0c28d4955e8
bxm <blues.xxx.man@gmail.com>
2021-08-02 10:38:07 +0100

    quicker

--- speed.sh
+++ speed.sh
@@ -1,9 +1,17 @@
+#!/usr/bin/env sh
 SMOOTH=${1:-3}
 ITER=${2:-5}
 
 while [ ${ITER} -gt 0 ] ; do
-ping 8.8.8.8 -c ${SMOOTH} \
-  | grep -oE time=[^\ ]+ \
-  | awk -F= '{n+=$NF ; i+=1} END {print n/i}'
-: $((ITER-=1))
+  : $((ITER-=1))
+
+  while [ ${SMOOTH} -gt 0 ] ; do
+    : $((SMOOTH-=1))
+    echo "  ${ITER} / ${SMOOTH}\r\c" >&2
+    ping 8.8.8.8 -c 1
+    sleep 0.1
+  done \
+    | grep -oE time=[^\ ]+ \
+    | awk -F= '{n+=$NF ; i+=1} END {print "\n"n/i}'
+
 done
