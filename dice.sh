de36cdb831f16d994657d81769f40133ef987010
bxm <blues.xxx.man@gmail.com>
2021-08-06 21:06:57 +0000

    improve face def

--- dice.sh
+++ dice.sh
@@ -2,17 +2,20 @@ rand(){
   strings /dev/random | grep -Eo "[1-6]" | head -1
 }
 
-roll(){
-  #i=$((RANDOM % 6 + 1))
-  i=$(rand)
-  set -- \
+size1(){
+  echo \
     ...../..o../..... \
     o..../...../....o \
     o..../..o../....o \
     o...o/...../o...o \
     o...o/..o../o...o \
     o...o/o...o/o...o
+}
 
+roll(){
+  #i=$((RANDOM % 6 + 1))
+  i=$(rand)
+  set -- $DIE
   eval r="\$$i"
   r="${r//\// }"
   hline=".${r// *}."
@@ -33,11 +36,12 @@ basic(){
 }
 
 main(){
+  DIE="$(size1)"
   while true ; do
     for x in a a a a a ; do
       roll
       echo Rolling...
-      sleep 0.1
+      sleep 0.15
     done
     roll
     unset ROLL
