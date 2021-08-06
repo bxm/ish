b565bcab315659c930ec59b2b6c696907f8ad6ff
bxm <blues.xxx.man@gmail.com>
2021-08-06 15:11:42 +0000

    basic dotted face

--- dice.sh
+++ dice.sh
@@ -1,4 +1,18 @@
 roll(){
+  i=$((RANDOM % 6 + 1))
+  set -- \
+    .../.o./... \
+    o../.../..o \
+    o../.o./..o \
+    o.o/.../o.o \
+    o.o/.o./o.o \
+    o.o/o.o/o.o
+
+  eval r="\$$i"
+  r="${r//\// }"
+  printf "%s\n%s\n%s\n\n" $r | sed 's/[.]/ /g'
+}
+basic(){
   v=$((RANDOM % 6 + 1))
   set -- one two three four five six
   eval echo $v \$$v
@@ -6,11 +20,6 @@ roll(){
 
 main(){
 
-roll 
-roll 
-roll 
-roll 
-roll 
 roll 
 }
 main 
