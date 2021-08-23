85a58270dca22ac6948c834e82a39e40ac9d1a55
bxm <blues.xxx.man@gmail.com>
2021-08-23 07:06:34 +0000

    bench does time itself

--- bench.sh
+++ bench.sh
@@ -1,9 +1,18 @@
-foo() {
-for x in $s ; do
-  $*
-done
+#!/bin/sh
+loop() {
+    s="$(seq 1 30000)"
+  for x in $s ; do
+    "$@"
+  done
 }
 
-s="$(seq 1 30000)"
+main(){
+  if [ "$1" = --spawn ] ; then
+    shift
+    loop "$@"
+  else
+    time "$0" --spawn "$@"
+  fi
+}
 
-foo $*
+main "$@"
