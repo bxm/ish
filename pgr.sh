21ab5230cb4719e99534b466edfbdc09862995f6
bxm <blues.xxx.man@gmail.com>
2021-08-24 11:39:08 +0000

    protect vars

--- pgr.sh
+++ pgr.sh
@@ -10,21 +10,21 @@ main(){
   get_tty
   FULL=$(( LINES * 8 / 10)) # actually 80% of tty
   HALF=$(( LINES * 45 / 100 ))
-  PAGE=$FULL
+  PAGE=${FULL}
   sed 's:\\:\\\\:g' | while read L ; do
     : $((i++))
     echo "${L:- }"
-    [ $i -lt $PAGE ] && continue
+    [ ${i} -lt ${PAGE} ] && continue
     while true ; do
       read -s -n1 -p% <&1
       echo -e "\r\c"
-      case "$REPLY" in
+      case "${REPLY}" in
         ([qQ]) break 2 ;;
         ($'\r') : $((i--)) ; break ;;
         ([A-D]) : $((i--)) ; break ;;
         ([1-9]) : $((i-=REPLY)) ; break ;;
-        ([hH]) PAGE=$HALF i=0 ; break ;;
-        ([fF]) PAGE=$FULL i=0 ; break ;;
+        ([hH]) PAGE=${HALF} i=0 ; break ;;
+        ([fF]) PAGE=${FULL} i=0 ; break ;;
         ([a-z\ ]) i=0 ; break ;;
         ([A-Z[]) : ;;
         (*) : ;; #$((i--)) ; break ;;
