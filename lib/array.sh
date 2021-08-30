bbf1253557e22e5d2da4f0ab22c99b03bb68cf25
bxm <blues.xxx.man@gmail.com>
2021-08-30 21:50:58 +0000

    add dependency, bit hacky

--- lib/array.sh
+++ lib/array.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env sh
 
+if ! type adlib 1>/dev/null 2>&1 ; then
+  adlib(){
+    local realname="$(readlink -f "$0")"
+    local libdir="${realname%/*}/lib"
+    while [ $# -gt 0 ] ; do
+      local libname="${1%.sh}.sh"
+      source "$libdir/$libname" || continue
+      debug added "$libdir/$libname"
+      shift
+    done
+  }
+fi
+
+adlib debug
+
 is_array(){
   # check if passed array exists
   # TODO could also validate all elements exist
