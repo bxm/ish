ebceb24ec83d7e52f1d27e32000ce9722d74dbe1
bxm <blues.xxx.man@gmail.com>
2021-08-31 17:27:05 +0000

    add variable debug

--- lib/debug.sh
+++ lib/debug.sh
@@ -2,5 +2,14 @@
 
 debug(){
   ${DEBUG:-false} || return 0
-  printf "## DEBUG ## >>%s<<\n" "$*" >&2
+  case "$1" in
+    (-v) 
+      shift
+      while [ $# -gt 0 ] ; do
+        eval debug "$1: \$$1"
+        shift
+      done
+      ;;
+    (* ) printf "## DEBUG: ## >>%s<<\n" "$*" >&2 ;;
+  esac
 }
--- testlib.sh
+++ testlib.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
-#adlib(){
-#  local realname="$(readlink -f "$0")"
-#  local libdir="${realname%/*}/lib"
-#	while [ $# -gt 0 ] ; do
-#		local libname="${1%.sh}.sh"
-#		source "$libdir/$libname" || continue
-#    debug added "$libdir/$libname"
-#		shift
-#	done
-#}
-#
-#DEBUG=true
-#adlib debug
-#DEBUG=true debug foo
-#
-(return 0) && sourced=true || sourced=false
-cat /proc/$$/cmdline | grep -oE "[\ -~]+"
+adlib(){
+  local realname="$(readlink -f "${0}")"
+  local libdir="${realname%/*}/lib"
+	while [ $# -gt 0 ] ; do
+		local libname="${1%.sh}.sh"
+		source "${libdir}/${libname}" || continue
+    debug added "${libdir}/${libname}"
+		shift
+	done
+}
+
+DEBUG=true
+adlib debug
+foo=bar
+DEBUG=true debug foo
+debug -v DEBUG foo
+
