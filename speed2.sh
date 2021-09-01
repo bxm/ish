8b38737c54dd961b533c88b35fa9323e634878a3
bxm <blues.xxx.man@gmail.com>
2021-09-01 19:39:55 +0000

    dedupe

--- speed2.sh
+++
@@ -1,5 +0,0 @@
-unset REPLY
-while read -s -n1 -t0.2 REPLY || true ; do
-  [ "$REPLY" = q ] && break
-  time wget http://example.com/ -T3 -o /dev/null -O /dev/null 2>&1 | grep real
-done | awk '{gsub(/s/,"",$NF); print $NF}' 
