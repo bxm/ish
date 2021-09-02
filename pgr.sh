992c2f718bcc03cda515258ef056996fe87dce1e
bxm <blues.xxx.man@gmail.com>
2021-09-02 23:02:41 +0000

    faff

--- pgr.sh
+++ pgr.sh
@@ -15,7 +15,7 @@ adlib(){
 
 prompt(){
   while true ; do
-    printf "${WHITE}%s${NC}" %
+    printf "${WHITE}${INV_ON}%%${NC}"
     read -s -n1 <&1
     echo -e "\r           \r\c"
     case "${REPLY}" in
