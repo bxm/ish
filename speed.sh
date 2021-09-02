852c6af37432eeee58f19d1c70cac01c25ec1bbb
bxm <blues.xxx.man@gmail.com>
2021-09-02 06:39:08 +0000

    make sleepy function

--- speed.sh
+++ speed.sh
@@ -24,7 +24,7 @@ speed_ping(){
     while [ ${SMOOTH} -gt 0 ] ; do
       : $((SMOOTH-=1))
       ping 8.8.8.8 -c 1
-      sleep 0.1
+      sleepy 0.1 | break 2
     done \
       | grep -oE time=[^\ ]+ \
       | awk -v "max=$smooth" -F= '{n+=$NF ; i+=1 ; if (i==max) {print int(n/i)}}' | bar 15
@@ -33,22 +33,30 @@ speed_ping(){
   done
 }
 
+sleepy(){
+  debug sleepy "$@"
+  local sleep_time="${1:-1}"
+  local reply
+  debug -v sleep_time
+  read -s -n1 -t$sleep_time reply
+  debug -v REPLY
+  ! [ "${reply}" = q ]
+}
+
 speed_dl(){
   debug speed_dl "$@"
   local url=''
+  local min=0
   case "${1:-small}" in
     (s*) url='http://example.com/' ;;
-    (m*) url='http://speedtest.tele2.net/1MB.zip' ;;
-    (l*) url='http://212.183.159.230/5MB.zip' ;;
+    (m*) url='http://speedtest.tele2.net/1MB.zip' min=25 ;;
+    (l*) url='http://212.183.159.230/5MB.zip' min=35 ;;
   esac
   debug -v url
   : "${url:?}"
-  unset REPLY
-  while read -s -n1 -t0.2 REPLY || true ; do
-    debug -v REPLY
-    [ "${REPLY}" = q ] && break
+  while sleepy 0.5 ; do
     time wget "$url" -T3 -o /dev/null -O /dev/null 2>&1 | grep real
-  done | awk '{gsub(/s/,"",$NF); print $NF*100}' | bar 25
+  done | awk '{gsub(/s/,"",$NF); print $NF*100}' | bar $min
 }
 
 bar(){
