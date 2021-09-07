c6f07562170477801b4dddb2dcc450557a0bb3ae
bxm <blues.xxx.man@gmail.com>
2021-09-07 09:13:37 +0100

    noop

--- golf/bug.sh
+++ golf/bug.sh
@@ -1,2 +1,2 @@
 # https://codegolf.stackexchange.com/questions/129523/it-was-just-a-bug
-for p in `seq 9 -1 2;seq 9`;do seq -s"`printf "%${p}s-"`" 10 ;done|sed s/10/0/\;s/.-//g
+for p in `seq 9 -1 2;seq 9`;do seq -s"`printf "%${p}s_"`" 10 ;done|sed s/10/0/\;s/._//g
