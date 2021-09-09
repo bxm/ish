62e20e71b4915ecacbf169ddf6219cf444f62a79
bxm <blues.xxx.man@gmail.com>
2021-09-09 20:47:27 +0000

    golf bug -4

--- golf/bug.sh
+++ golf/bug.sh
@@ -1,2 +1,2 @@
 # https://codegolf.stackexchange.com/questions/129523/it-was-just-a-bug
-for p in `seq -- -8 8`;do seq -s"`printf "%${p}s_"`" 10;done|sed s/_1*//g
+seq -- -8 8|xargs -I: sh -c 'seq -s"`printf "%:s_"`" 10'|sed s/_1*//g
