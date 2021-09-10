b9baec69cc36154f9bb9390be6962522224b9c3e
bxm <blues.xxx.man@gmail.com>
2021-09-10 08:03:13 +0100

    add output hash for reference

--- golf/bug.sh
+++ golf/bug.sh
@@ -1,2 +1,3 @@
 # https://codegolf.stackexchange.com/questions/129523/it-was-just-a-bug
+# 2399c533b9c5fd67050a9d2ae3aab0f8  output hash
 seq -- -8 8|xargs -I: sh -c 'seq -s"`printf "%:s_"`" 10'|sed s/_1*//g
