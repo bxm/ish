cf847f4adccacabf677006720bf0f76401c101c2
bxm <blues.xxx.man@gmail.com>
2021-09-10 15:19:09 +0100

    improved fzbz

--- golf/fzbz.sh
+++ golf/fzbz.sh
@@ -1,1 +1,2 @@
-for i in $(seq 1 100);do([ $((i%5)) = 0 ]&&j=Buzz;[ $((i%3)) = 0 ]&&j=Fizz$j;echo ${j-$i});done
+#for i in $(seq 100);do([ $((i%5)) = 0 ]&&j=Buzz;[ $((i%3)) = 0 ]&&j=Fizz$j;echo ${j-$i});done
+f(){ [ $((i%$1)) = 0 ]&&j=$2zz$j;};for i in $(seq 100);do(f 5 Bu;f 3 Fi;echo ${j-$i});done
