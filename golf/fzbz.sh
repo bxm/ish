b9acd053b4ca07dbc536efe7723b06cb6e1fcf37
bxm <blues.xxx.man@gmail.com>
2021-09-10 22:26:03 +0000

    more fzbz attempts

--- golf/fzbz.sh
+++ golf/fzbz.sh
@@ -1,2 +1,5 @@
 #for i in $(seq 100);do([ $((i%5)) = 0 ]&&j=Buzz;[ $((i%3)) = 0 ]&&j=Fizz$j;echo ${j-$i});done
-f(){ [ $((i%$1)) = 0 ]&&j=$2zz$j;};for i in `seq 100`;do(f 5 Bu;f 3 Fi;echo ${j-$i});done
+#f(){ [ $((i%$1)) = 0 ]&&j=$2zz$j;};for i in `seq 100`;do(f 5 Bu;f 3 Fi;echo ${j-$i});done
+#_0(){ :;};f(){(_$((0/(i%$1))))2>f||j=$2zz$j;};for i in `seq 100`;do(f 5 Bu;f 3 Fi;echo ${j-$i});done
+#f(){(: $((0/(i%$1))))2>f||j=$2zz$j;};for i in `seq 100`;do(f 5 Bu;f 3 Fi;echo ${j-$i});done
+f(){([ $((0/(i%$1))) ])||j=$2zz$j;};for i in `seq 100`;do(f 5 Bu;f 3 Fi;echo ${j-$i});done
