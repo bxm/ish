52d901384f93f373358ab4f1cc647cc6354f8d9e
bxm <blues.xxx.man@gmail.com>
2021-09-11 19:28:53 +0000

    add mid.sh

---
+++ golf/mid.sh
@@ -0,0 +1,17 @@
+sed -r 's/ /   /;s/[^ ]+ (.+) [^ ]+/\1/;s/ +//'
+
+exit
+Input: Samantha Vee Hills
+Output: Vee
+
+Input: Bob Dillinger
+Output: (empty string or newline)
+
+Input: John Jacob Jingleheimer Schmidt
+Output: Jacob Jingleheimer
+
+Input: Jose Mario Carasco-Williams
+Output: Mario
+
+Input: James Alfred Van Allen
+Output: Alfred Van
