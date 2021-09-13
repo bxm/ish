234647ddb52ac101ee53ce30bea3f25b618f85a8
bxm <blues.xxx.man@gmail.com>
2021-09-13 05:47:16 +0000

    ack wip

--- ack.sh
+++ ack.sh
@@ -11,9 +11,22 @@ adlib(){
 	done
 }
 
+process_args(){
+  debug process_args "$@"
+  ICASE=false
+  while [ $# -gt 0] ; do
+    case "$1" in
+      (-i) ICASE=true ;;
+    esac
+    shift
+  done
+}
+
 main(){
   debug main "$@"
   # TODO support -g, -l, -i, -hH
+  # implicit ignore case for -g?
+  # authentic piped -g w/ -x or "lazy" non piped?
   # fancy output? -b option for bare?
   # colour inline matches?
   find . -mindepth 1 -type f ! -path */.git/* ! -path */.git | sort
