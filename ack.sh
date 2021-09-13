7bbc5a8b5decae164870868e0a6b4bfef5c06e4c
bxm <blues.xxx.man@gmail.com>
2021-09-13 11:27:10 +0100

    ack wip more opts config

--- ack.sh
+++ ack.sh
@@ -16,6 +16,8 @@ process_args(){
   ICASE=false
   LIST=false
   HEAD=true
+  FILEGREP=false
+  local context='GEN'
   local opt
   #while [ $# -gt 0] ; do
     #param="$1"
@@ -24,12 +26,22 @@ process_args(){
 # eat file/rx params
 # handling for -- ?
     #while [ ${#param}
-
-  for opt in $(getopt -ghil -- "$@")
-    case "$o" in
-      (-i) ICASE=true ;;
-      (-l) LIST=true ;;
-      (-H) HEAD=false ;;
+  local args="$(getopt -ghil -- "$@")"
+  set -- ${args}
+  while [ $# -gt 0 ] ; do
+    case "${context}/${opt}" in
+      (GEN/-h ) HEAD=false ;;
+      (GEN/-g ) FILEGREP=true ;;
+      (GEN/-i ) ICASE=true ;;
+      (GEN/-l ) LIST=true ;;
+      (GEN/-- ) context=PARAM ;;
+      (GEN/-* )  ;; # unhandled opt
+      (GEN/*  )  ;; # grab danglers as rx, files
+      (PARAM/*)  ;; # in PARAM context fill rx (if empty), and files with everything else
+        # TODO do something with non opt args?
+        # The norm actually seem to be to break on -- and I guess
+        # have another routine for files etc but going it with
+        # context seems better, so we can 
     esac
   done
 }
@@ -39,10 +51,12 @@ process_args(){
 # authentic piped -g w/ -x or "lazy" non piped?
 # fancy output? -b option for bare?
 # colour inline matches?
+list_files(){
+  find . -mindepth 1 -type f ! -path */.git/* ! -path */.git | sort
+}
 
 main(){
   debug main "$@"
-  find . -mindepth 1 -type f ! -path */.git/* ! -path */.git | sort
 }
 
 adlib debug decor
