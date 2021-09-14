7984f0fba981fd4334be0416f84051eac5dc6e4b
bxm <blues.xxx.man@gmail.com>
2021-09-14 06:55:19 +0000

    faff

--- ack.sh
+++ ack.sh
@@ -18,9 +18,9 @@ usage(){
 non_opt_args(){
   debug non_opt_args "$@"
   # fill rx (if empty), and files with everything else
-  [ -z "$CONTENT_RX" ] && CONTENT_RX="$1" && shift
+  [ -z "${EXPRESSION}" ] && EXPRESSION="${1}" && shift
   array_push FILES "$@"
-  debug -v CONTENT_RX FILES_S FILES_E
+  debug -v EXPRESSION FILES_S FILES_E
 }
 
 process_args(){
@@ -29,7 +29,7 @@ process_args(){
   LIST=false
   HEAD=true
   FILEGREP=false
-  CONTENT_RX=''
+  EXPRESSION=''
   array_new FILES
   local context='DEF'
   #while [ $# -gt 0] ; do
@@ -38,7 +38,6 @@ process_args(){
 # maybe just use getopt?
 # eat file/rx params
 # handling for -- ?
-    #while [ ${#param}
   local args="$(getopt -n "${RED}warning${_NC_}" -o gHil -- "$@")"
   eval set -- "${args}"
   while [ $# -gt 0 ] ; do
