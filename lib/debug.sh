c1312f23f63564bd04306e53ac49459fc95466e3
bxm <blues.xxx.man@gmail.com>
2021-09-17 16:20:54 +0000

    add debug -f, debug includes coloured pid

--- lib/debug.sh
+++ lib/debug.sh
@@ -2,14 +2,31 @@
 
 debug(){
   ${DEBUG:-false} || return 0
-  case "$1" in
+  case "${1}" in
+    (-f)
+      local func="$2"
+      shift 2
+      debug "${RED}${func}${BLUE} $*${_NC_}"
+      ;;
     (-v)
       shift
       while [ $# -gt 0 ] ; do
-        eval debug "$1: \>\>\$$1\<\<"
+        eval debug "${1}: \>\>\$${1}\<\<"
         shift
       done
       ;;
-    (* ) printf "## DEBUG [$$] ## %s\n" "$*" >&2 ;;
+    (* )
+
+      printf "## DEBUG [$(pid_colour)$$${_NC_}] ## %s\n" "$*" >&2 ;;
   esac
 }
+
+pid_colour() {
+  local cols='LGREY BLUE BROWN LBLUE GREEN LCYAN GREY LPURPLE RED WHITE LRED CYAN YELLOW PURPLE '
+  set -- ${cols}
+  local pidmod=$(($$ % $# + 1))
+  eval col=\$${pidmod}
+  eval COL_PID_$$=\$${col}
+  eval printf "\${COL_PID_$$}"
+}
+
