5c0938cb0cd263e4f9ce5d770fe3cae2a54aafb4
bxm <blues.xxx.man@gmail.com>
2021-09-18 07:36:13 +0000

    golf obfzbz -4

--- golf/obfzbz.sh
+++ golf/obfzbz.sh
@@ -1,1 +1,1 @@
-v="   FiBu";for i in $(seq 100);do(for m in $(seq 6 -1 2|grep -v [642]);do [ $((i%m)) = 0 ]&&g=${v:$m:2}zz$g;done;echo ${g-$i});done
+v="gJeFiBu";for i in $(seq 100);do(for m in $(seq 6|grep -v [6421]);do [ $((i%m)) = 0 ]&&g=$g${v:$m:2}zz;done;echo ${g-$i});done
