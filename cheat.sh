19be4d31ca8eaaf411f9b9c9a7950caaaffaf2a1
bxm <blues.xxx.man@gmail.com>
2021-09-20 22:53:41 +0100

    move cheat sheet storage

--- cheat.sh
+++ cheat.sh
@@ -12,14 +12,16 @@ adlib(){
 }
 
 main(){
-  debug main "$@"
   set -e
-  local cheat_dir=${HOME}/cheat
-  local cheat=$cheat_dir/$1.txt
-  if [ -f ${cheat} ] ; then
-    cat ${cheat} | pgr
+  debug main "$@"
+  local realname="$(readlink -f "${0}")"
+  local mydir="${realname%/*}"
+  local cheat_dir="${mydir}/cheat"
+  local cheat="${cheat_dir}/${1}.txt"
+  if [ -f "${cheat}" ] ; then
+    cat "${cheat}" | pgr
   else
-    cd $cheat_dir
+    cd "${cheat_dir}"
     ls | sed 's/[.]txt$//g'
   fi
 }
