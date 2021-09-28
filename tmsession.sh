1a37cb528e070ea6302819baea4ed4649d77563d
bxm <blues.xxx.man@gmail.com>
2021-09-28 08:38:39 +0100

    fiddle tmsession

--- tmsession.sh
+++ tmsession.sh
@@ -1,5 +1,6 @@
 tmux start-server
-tmux new-session -d -s X -n home -c ~
-tmux new-window -t X -n code -c ~/code/ish
+#tmux new-session -d -s X -n home -c ~
+#tmux new-window -t X -n code -c ~/code/ish
+tmux new-session -d -s X -n code -c ~/code/ish
 tmux new-window -t X -n nvim -c ~/code/ish nvim
 tmux attach
