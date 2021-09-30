d6732b93dcbc6bf52666824f7d40f08b00dfc44f
bxm <blues.xxx.man@gmail.com>
2021-09-30 22:02:17 +0100

    modem two methods

--- modem.sh
+++ modem.sh
@@ -11,9 +11,10 @@ adlib(){
   done
 }
 
-main(){
-  debug -f main "$@"
+convoluted(){
+  debug -f native "$@"
   m=-rwxr-x--x
+  echo ${m:1}
   seq 1 9 | while read i ; do
     : $((q=((9-i)%3),x=q<<1))
     x=${x/0/1}
@@ -21,14 +22,23 @@ main(){
     k=${j/[^-]/$x}
     k=${k/-/0}
     [ $((q>>1)) = 1 ] && f=$k || : $((f+=k))
-    printf "$x $q $j $k"
-    [ $q = 0 ] && echo " $f" || echo
+    #printf "$x $q $j $k"
+    #[ $q = 0 ] && echo " $f" || echo
+    [ $q = 0 ] && printf "$f"
   done
+  echo
+}
 
-
+splitpipes() {
+  m=-rwxr-x--x
+  j=0
+  echo ${m:1}
+  echo ${m:1} | tr "rwx-" "4210" | sed -r 's/.{3}/&:/g;s/./&\n/g' | while read i ; do if [ "$i" = : ] ; then printf $j ; j=0 ; else : $((j+=i)) ; fi ; done
+  echo
 }
 
 adlib debug install
 
-main "$@"
+convoluted "$@"
+splitpipes
 
