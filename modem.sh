e51f081a72868023d2170283c69c5959ed5955c5
bxm <blues.xxx.man@gmail.com>
2021-09-30 20:48:44 +0100

    modem working but indecipherable

--- modem.sh
+++ modem.sh
@@ -13,7 +13,17 @@ adlib(){
 
 main(){
   debug -f main "$@"
-  m=-rwxr-x--x ; seq 1 9 | while read i ; do : $((q=((9-i)%3),x=q<<1)) ; [ $x = 0 ] && x=1 ; j=${m:$i:1} ; k=${j/[^-]/$x}; k=${k/-/0};echo $x $q $j $k ; done
+  m=-rwxr-x--x
+  seq 1 9 | while read i ; do
+    : $((q=((9-i)%3),x=q<<1))
+    x=${x/0/1}
+    j=${m:$i:1}
+    k=${j/[^-]/$x}
+    k=${k/-/0}
+    [ $((q>>1)) = 1 ] && f=$k || : $((f+=k))
+    printf "$x $q $j $k"
+    [ $q = 0 ] && echo " $f" || echo
+  done
 
 
 }
