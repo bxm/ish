6cfe4e30a8397bae73824c558de69d965a9ab113
bxm <blues.xxx.man@gmail.com>
2021-10-03 22:25:37 +0100

    tm fix dir for ack

--- tmsession.sh
+++ tmsession.sh
@@ -2,5 +2,5 @@ tmux start-server
 #tmux new-session -d -s X -n home -c ~
 #tmux new-window -t X -n code -c ~/code/ish
 tmux new-session -d -s X -n code -c ~/code/ish
-tmux new-window -t X -n nvim -c ~/code/ish nvim -p ${1:+$(ack -g "${*// /|}")}
+tmux new-window -t X -n nvim -c ~/code/ish nvim -p ${1:+$(ack -g "${*// /|}" ~)}
 tmux attach
