1b7f17de7b19ce9b83836b3b70176358ee902dbe
bxm <blues.xxx.man@gmail.com>
2021-10-04 09:35:19 +0100

    tmux session select 0

--- tmsession.sh
+++ tmsession.sh
@@ -1,6 +1,8 @@
+SESSION=X
 tmux start-server
-#tmux new-session -d -s X -n home -c ~
-#tmux new-window -t X -n code -c ~/code/ish
-tmux new-session -d -s X -n code -c ~/code/ish
-tmux new-window -t X -n nvim -c ~/code/ish nvim -p ${1:+$(ack -g "${*// /|}" ~)}
+#tmux new-session -d -s ${SESSION} -n home -c ~
+#tmux new-window -t ${SESSION} -n code -c ~/code/ish
+tmux new-session -d -s ${SESSION} -n code -c ~/code/ish
+tmux new-window -t ${SESSION} -n nvim -c ~/code/ish nvim -p ${1:+$(ack -g "${*// /|}" ~)}
+tmux select-window -t ${SESSION}:0
 tmux attach
