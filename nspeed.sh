b8c1ff5bbf9afdfb3dca0236c81e49e0d227414f
bxm <blues.xxx.man@gmail.com>
2021-10-15 20:23:05 +0100

    add nspeed.sh

---
+++ nspeed.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+adlib(){
+  local realname="$(readlink -f "${0}")"
+  local libdir="${realname%/*}/lib"
+  while [ $# -gt 0 ] ; do
+    local libname="${1%.sh}.sh"
+    source "${libdir}/${libname}" || continue
+    debug added "${libdir}/${libname}"
+    shift
+  done
+}
+
+main(){
+  debug -f main "$@"
+ping 8.8.8.8 | awk -vcols=54 -vtimepc=0 -vbarcols=0 -vmod=1 -vtotal=0 -vi=0 -vmax=0 '/time=/ {i++ ; time= ; gsub(/[^[:digit:]]/,"",time) ; total+=time ; avg=total/i ; if (time>max) {max=time*1.1;print "max increased"} ; timepc=time/max ; barcols=timepc*cols;for(c=0;c<barcols;c++) {printf "#"}; printf "\n"}'
+}
+
+adlib debug install
+
+main "$@"
+
