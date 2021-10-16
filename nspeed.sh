436a817a2cf874252e05c1efa7432b17f31a3a69
bxm <blues.xxx.man@gmail.com>
2021-10-16 09:28:56 +0100

    nspeed tidy wip

--- nspeed.sh
+++ nspeed.sh
@@ -3,7 +3,8 @@
 adlib(){
   local realname="$(readlink -f "${0}")"
   local libdir="${realname%/*}/lib"
-  while [ $# -gt 0 ] ; do
+  while [ $# -gt 0 ]
+do
     local libname="${1%.sh}.sh"
     source "${libdir}/${libname}" || continue
     debug added "${libdir}/${libname}"
@@ -13,7 +14,21 @@ adlib(){
 
 main(){
   debug -f main "$@"
-ping 8.8.8.8 | awk -vcols=$(tput cols) -vtimepc=0 -vbarcols=0 -vmod=1 -vtotal=0 -vi=0 -vmax=0 '/time=/ {i++ ; time=$7 ; gsub(/[^[:digit:]]/,"",time) ; total+=time ; avg=total/i ; if (time>max) {max=time*1.1;print "max increased"} ; timepc=time/max ; barcols=timepc*cols;for(c=0;c<barcols;c++) {printf "#"}; printf "\n"}'
+ping 8.8.8.8 | awk \
+  -vcols=$(tput cols) -vtimepc=0 -vbarcols=0 \
+  -vtotal=0 -vi=0 -vmax=0 \
+    '/time=/ {
+      i++
+time=$7
+gsub(/[^[:digit:]]/,"",time)
+total+=time
+avg=total/i
+if (time>max) {max=time*1.1
+print "max increased"}
+timepc=time/max
+barcols=timepc*cols
+for(c=0;c<barcols;c++) {printf "#"}
+printf "\n"}'
 }
 
 adlib debug install
