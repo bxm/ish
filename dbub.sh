5c1f216444992ea9ce995601a4c03d929a492f07
bxm <blues.xxx.man@gmail.com>
2024-05-06 17:30:03 +0000

    wip dbub, aiming for mvp rn

--- dbub.sh
+++ dbub.sh
@@ -16,15 +16,6 @@ make_list(){
   shuf -i1-22 > ${list}
 }
 
-main(){
-  debug -f main "$@"
-  local list=/tmp/list
-  make_list
-  cat ${list}
-
-  
-}
-
 block(){
   case $1 in
     (half)
@@ -34,12 +25,33 @@ block(){
   esac
 }
 
-column(){
+col(){
   local length=$1
   local full=$((length / 2))
   local half=$((length % 2))
   local col
-  printf -v '%s%s' $(block full) $(block half)
+  #local seq="{0..$full}"
+  #printf %s. $seq
+  echo $length
+  #for i in ((i=1;i++;i>length)) ; do
+  test $half -eq 1 && col+=($(block half))
+  for i in $(seq 1 $full) ; do
+    col+=($(block full))
+  done
+  printf %s"\n" ${col[*]}
+  #printf '%s' $(block full) $(block half)
+}
+
+main(){
+  debug -f main "$@"
+  local list=/tmp/list
+  make_list
+  #cat ${list}
+  col 1
+  col 2
+  col 3
+  col 15
+  
 }
 
 adlib debug install
