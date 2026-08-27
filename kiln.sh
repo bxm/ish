f3fc0a68a64d67c11d293d2e7ab807acc356462d
bxm <blues.xxx.man@gmail.com>
2026-08-27 13:45:34 +0100

    fix cd

--- kiln.sh
+++ kiln.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 adlib(){
-  local realname="$(readlink -f "${0}")"
+  realname="$(readlink -f "${0}")"
   local libdir="${realname%/*}/lib"
   while [ $# -gt 0 ] ; do
     local libname="${1%.sh}.sh"
